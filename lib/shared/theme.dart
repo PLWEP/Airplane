cadae0326e6eae3b75f400a82e494abc47db1357
PLWEP <langgeng86@gmail.com>
2022-09-14 19:44:52 +0700

    first commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color(0xff5c40cc);
+Color blackColor = const Color(0xff1f1449);
+Color whiteColor = const Color(0xffffffff);
+Color greyColor = const Color(0xff9698a9);
+Color greenColor = const Color(0xff0ec3ae);
+Color redColor = const Color(0xffeb70a5);
+Color backgroundColor = const Color(0xfffafafa);
+Color inactiveColor = const Color(0xffd8d7ec);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor);
+TextStyle redTextStyle = GoogleFonts.poppins(color: redColor);
+TextStyle purpleTextStyle = GoogleFonts.poppins(color: primaryColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
