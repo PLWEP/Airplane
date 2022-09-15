267f37944922ab535a0b8fc1d4c6e3d74fb64723
PLWEP <langgeng86@gmail.com>
2022-09-15 21:44:58 +0700

    fix minor bug

--- lib/ui/widgets/custom_bottom_nav_item.dart
+++ lib/ui/widgets/custom_bottom_nav_item.dart
@@ -1,5 +1,5 @@
 import 'package:airplane/shared/theme.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CustomBottomNavItem extends StatelessWidget {
   final bool isSelected;
